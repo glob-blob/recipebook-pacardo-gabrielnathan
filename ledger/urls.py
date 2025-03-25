2f7c59952b01f5407f4706238acc90050badd0b7
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-03-25 10:51:27 +0800

    Added the necessary information to create new recipe on the user side and add images to exisiting recipes.

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RecipesListView, RecipesDetailView
+from .views import RecipesListView, RecipesDetailView, RecipeCreateView, RecipeImageUpdateView
 
 urlpatterns = [
     path('recipes/list', RecipesListView.as_view(), name="recipes-list"),
     path('recipe/<int:pk>', RecipesDetailView.as_view(), name='recipe'),
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe-create'),
+    path('recipe/<int:pk>/add_image', RecipeImageUpdateView.as_view(), name='recipe-update-img')
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -1,7 +1,12 @@
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Recipe
+from django.views.generic.edit import CreateView, UpdateView
+
+from .forms import RecipeImageForm
+from .models import Recipe, RecipeImage
 
 class RecipesListView(ListView):
     model = Recipe
@@ -11,3 +16,22 @@ class RecipesListView(ListView):
 class RecipesDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html'
+
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    fields = '__all__'
+
+
+class RecipeImageUpdateView(UpdateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = 'recipe_update_img.html'
+    
+    def get_object(self):
+        recipe = get_object_or_404(Recipe, pk=self.kwargs['pk'])
+        recipe_image, create = RecipeImage.objects.get_or_create(recipe=recipe)
+        return recipe_image
+    
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe', kwargs={ 'pk': self.object.recipe.pk })
