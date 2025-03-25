7cbeb98f2225738d77333edd5e3746de7b0a750c
glob-blob <gabriel.pacardo@student.ateneo.edu>
2025-03-25 10:50:21 +0800

    Modified Admin.py to include RecipeImage as an InLine to RecipeAdmin.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,24 +1,32 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
+
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
+
+class RecipeImageInLine(admin.TabularInline):
+    model = RecipeImage
+
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
     search_fields = ('name', )
 
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInLine]
 
     search_fields = ('name', )
 
     ('Details', {
-        'ingredients':
-            ('name', 'quantity')
+        'ingredients': [
+            ('name', 'quantity'), 'image'
+        ]
     })
 
 admin.site.register(Recipe, RecipeAdmin)
